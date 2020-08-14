a81347000779e761b3097b64e1822e4baa9a504c
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-14 15:55:47 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,8 +31,8 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-     matrix[assembled_matrix][row][column]
-     matrix[sorted_matrix][row][column]
+     [assembled_matrix][row][column]
+     [sorted_matrix][row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
